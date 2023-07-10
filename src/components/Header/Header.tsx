0ca661ef2051cbdc034d08d4e3acbfddf9ab7e9f
lordvader501 <60027612+lordvader501@users.noreply.github.com>
2023-07-10 20:29:23 +0530

    added rsponsive menu for mobile

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,17 @@
-import React, { useEffect } from 'react';
-import { Link, useLocation }from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import './Header.css';
 import { LOGO } from '../../utilities/constanats';
 import Search from '../SearchBox/Search';
 
 const HeaderLayout: React.FC = () => {
 	const currLocation = useLocation();
+	const [menuOpen, setMenuOpen] = useState(false);
+
 	useEffect(() => {
 		const navbar:HTMLElement |null = document.getElementById('nav-container');
 		const sticky = navbar?.offsetTop || 0;
+
 		const handleScroll = (): void => {
 			if (navbar && window.scrollY >= sticky) {
 				navbar.classList.add('sticky');
@@ -16,12 +19,17 @@ const HeaderLayout: React.FC = () => {
 				navbar.classList.remove('sticky');
 			}
 		};
+
 		window.addEventListener('scroll', handleScroll);
 		return () => {
 			window.removeEventListener('scroll', handleScroll);
 		};
 	}, []);
-	
+
+	const handleMenuToggle = (): void => {
+		setMenuOpen(!menuOpen);
+	};
+	console.log(currLocation.pathname);
 	return (
 		<header className="header">
 			<div className="logo-container">
@@ -37,9 +45,14 @@ const HeaderLayout: React.FC = () => {
 			<div className="search-container">
 				<Search />
 			</div>
-			<div className="navdiv">
-				<div id="nav-container" className='navcontainer'>
-					<nav className="navbar">
+			<div className='navdiv'>
+				<div id='nav-container' className={`navcontainer ${menuOpen ? 'mobile-menu-open' : ''}`}>
+					<nav className='navbar'>
+						<div className={`menu-toggle ${menuOpen ? 'open' : ''}`} onClick={handleMenuToggle}>
+							<div className="bar1"></div>
+							<div className="bar2"></div>
+							<div className="bar3"></div>
+						</div>
 						<ul className="nav-list">
 							<li className="nav-item">
 								<Link className={`nav-link ${(currLocation.pathname === '/pokelister') ? 'active':''}`} to="/pokelister">Home</Link>
